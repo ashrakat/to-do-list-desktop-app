6ab1e1f4df5c98d5b25f93687055b57b5cf5c0eb
Ashrakat <ashrakat.mokhtar@yahoo.com>
2017-07-13 20:37:39 +0200

    Update Addframe.java

--- src/Addframe.java
+++ src/Addframe.java
@@ -12,6 +12,8 @@ import java.awt.List;
 import java.awt.Window;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Addframe  extends Mainframe{
 
@@ -62,16 +64,64 @@ public class Addframe  extends Mainframe{
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
+		textField_1.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+			@Override
+			public void keyPressed(KeyEvent e) {
+			 int key = e.getKeyCode();
+			 if((key >= e.VK_0 && key <= e.VK_9) || (key >= e.VK_NUMPAD0 && key <= e.VK_NUMPAD9) ||
+					 key == KeyEvent.VK_SLASH || key == e.VK_BACK_SPACE)
+			 {
+				textField_1.setEditable(true);
+			 }
+			 else
+			 {
+				 textField_1.setEditable(false);
+			 }
+			}
+		});
 		textField_1.setColumns(10);
 		textField_1.setBounds(92, 158, 166, 26);
 		frmAddTask.getContentPane().add(textField_1);
 		
 		textField_2 = new JTextField();
+		textField_2.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int key = e.getKeyCode();
+				 if((key >= e.VK_0 && key <= e.VK_9) || (key >= e.VK_NUMPAD0 && key <= e.VK_NUMPAD9) || 
+					  key == e.VK_BACK_SPACE)
+				 {
+					textField_2.setEditable(true);
+				 }
+				 else
+				 {
+					 textField_2.setEditable(false);
+				 }
+			}
+		});
 		textField_2.setColumns(10);
 		textField_2.setBounds(92, 196, 166, 26);
 		frmAddTask.getContentPane().add(textField_2);
 		
 		textField_3 = new JTextField();
+		textField_3.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int key = e.getKeyCode();
+				 if((key >= e.VK_0 && key <= e.VK_9) || (key >= e.VK_NUMPAD0 && key <= e.VK_NUMPAD9) || 
+					  key == e.VK_BACK_SPACE)
+				 {
+					textField_3.setEditable(true);
+				 }
+				 else
+				 {
+					 textField_3.setEditable(false);
+				 }
+			}
+		});
 		textField_3.setColumns(10);
 		textField_3.setBounds(92, 234, 166, 26);
 		frmAddTask.getContentPane().add(textField_3);
